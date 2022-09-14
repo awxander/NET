c8c9ece9b11063da2b13b81ba65002ffa6499dd7
Alexander <sashok.tsibin@gmail.com>
2022-09-14 12:02:40 +0700

    trying not deprecated method joinGroup

--- lab1/untitled/src/UDPstaf/UDPDataExchange.java
+++ lab1/untitled/src/UDPstaf/UDPDataExchange.java
@@ -6,6 +6,7 @@ import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.MulticastSocket;
 import java.net.NetworkInterface;
+import java.net.SocketAddress;
 import java.util.Arrays;
 import java.util.List;
 
@@ -16,11 +17,18 @@ public class UDPDataExchange {
     private static List<InetAddress> addressList;
 
     public static void main(String[] args) {
-
+        int port = 6789;
         try (/*MulticastSocket sendSocket = new MulticastSocket(6789);*/
-             MulticastSocket receiveSocket = new MulticastSocket(6789);) {
-            InetAddress group = InetAddress.getByName("230.0.0.0");
-            receiveSocket.joinGroup(group);
+             MulticastSocket receiveSocket = new MulticastSocket(port);) {
+            InetAddress inetAddress = InetAddress.getByName("230.0.0.2");
+            if(!inetAddress.isMulticastAddress()){
+                System.out.println("not a multicast address");
+                System.exit(1);
+            }
+
+
+            NetworkInterface netInterface = NetworkInterface.getByInetAddress(inetAddress);
+            receiveSocket.joinGroup(new InetSocketAddress(inetAddress,port ), netInterface);
 //            receiveSocket.joinGroup(inetSocketAddress, nif);
 
 //            new SendThread(sendSocket, group).start();
