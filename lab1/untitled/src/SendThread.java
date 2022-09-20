2c5fe61776661804f007a4cacaf2785dae8eedff
Alexander <sashok.tsibin@gmail.com>
2022-09-20 12:38:28 +0700

    first lab done

---
+++ lab1/untitled/src/SendThread.java
@@ -0,0 +1,44 @@
+
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+
+public class SendThread extends Thread{
+
+    MulticastSocket multicastSocket;
+    InetAddress inetAddress;
+    int port;
+
+    public SendThread(MulticastSocket multicastSocket, InetAddress inetAddress, int port) {
+        this.multicastSocket = multicastSocket;
+        this.inetAddress = inetAddress;
+        this.port = port;
+    }
+
+    @Override
+    public void run() {
+
+        while(true){
+            String msg = "";
+            DatagramPacket packet = new DatagramPacket(msg.getBytes(), msg.length(),
+                    inetAddress, port);
+            try {
+                multicastSocket.send(packet);
+            } catch (IOException e) {
+                e.printStackTrace();
+                break;
+            }
+
+            synchronized (Thread.currentThread()){
+                try {
+                    wait(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    break;
+                }
+            }
+        }
+    }
+}
